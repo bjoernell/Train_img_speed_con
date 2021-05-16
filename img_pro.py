1731354df73148eb92ac9d382faad6a8d615b5a0
bjoernell <bjoern.ellwert@gmail.com>
2021-05-16 22:43:38 +0200

    added video, changed from gray binary mask to hsv binary mask

--- img_pro.py
+++ img_pro.py
@@ -1,11 +1,12 @@
-from cv2 import cv2 as cv
+import cv2 as cv
 import numpy as np
 import math
 
-cap = cv.VideoCapture('Resources/rail2.avi')
-#cap = cv.VideoCapture(0)
-largest_contour_index = -1
+#cap = cv.VideoCapture('Resources\rail_without_carpet.avi')
+cap = cv.VideoCapture(0)
 
+#setup values
+largest_contour_index = -1
 ret, frame = cap.read()
 frame_height = frame.shape[0]
 frame_width = frame.shape[1]
@@ -13,39 +14,20 @@ half_frame_height = frame_height * 0.5
 half_frame_width = frame_width * 0.5
 print(str(frame_height) +  "x" + str(frame_width))
 
-mask_line_mat = np.zeros((frame_height, frame_width), dtype=np.uint8)
-mask_trafficlight = np.zeros((frame_height,frame_width,3), dtype=np.uint8)
-
-rect_line = cv.rectangle(mask_line_mat,(0,180),(frame_width,frame_height), (255,255,255), cv.FILLED)
-rect_trafficlight = cv.rectangle(mask_trafficlight,(423,0),(frame_width,frame_height),(255,255,255),cv.FILLED)
-
-print(str(mask_line_mat))
-print(str(mask_trafficlight.shape))
-
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     
-    # frame to gray and binary image
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    gray_inv = cv.bitwise_not(gray)
-    ret, binary = cv.threshold(gray_inv,150,170,cv.THRESH_BINARY)
-    
-    #frame to hsv and creating a green mask
-    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_green = np.array([35, 140, 60]) 
-    upper_green = np.array([255, 255, 180])
-    mask_trafficlight = cv.inRange(hsv_frame, lower_green, upper_green) 
- 
-    #mask erstellen
-    mask_line_binary = cv.bitwise_and(mask_line_mat, binary)
-    #mask_trafficlight_color = cv.bitwise_and(mask_trafficlight, hsv_frame)
+    #gen hsv img and mask
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lowGray = np.array([3,40,109])
+    highGray = np.array([28,94,216])
+    mask = cv.inRange(hsv, lowGray, highGray)
 
-    contours, hierarchy = cv.findContours(mask_line_binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    
+    #Größte Kontur finden    
+    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     largest_contour_area = 0
 
-    #Größte Kontur finden
     for i in range (len(contours)):
         if len(contours) != 0:
             moments = cv.moments(contours[i])
@@ -71,24 +53,23 @@ while(True):
     #ox = np.int0(box)
     #cv.drawContours(frame,[box],0,(0,0,255),2)
     
-    #Berechnung des Winkels der Schienen zur Kamera
+    #angle calculation
     if cX1 != 176:
         m = (640-cY1)/(176-cX1)
         degree = abs(math.degrees(math.atan(m)))
         
-        #Berechung der Motoransteuerung
+    #speed calculation
         motor_pwm = 1.68 * degree     #Max Geschwindigkeit liegt bei PWM = 150
         print(str(degree) + (" ; ") + str(motor_pwm))
     
     
 
     # Display the resulting frame
-    cv.imshow('binary', binary)
-    cv.imshow('mask_line binary', mask_line_binary)
+    cv.imshow('binary', mask)
+    cv.imshow('hsv', hsv)
     cv.imshow('original',frame)
-    cv.imshow('mask_trafficlight',mask_trafficlight)
 
-    
+    #stop when q pressed
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
     
